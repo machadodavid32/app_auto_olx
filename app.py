f2f8230560abbf8dc0e01d06a7b91ada505b69d6
David <machadodavid32@gmail.com>
2023-04-13 20:19:16 -0300

    Adicionado input para pesquisa. Próximo passo é adicionar escolha dos Estados

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from time import sleep
 import os
 import PySimpleGUI as sg
 from threading import Thread
+import random
 
 
 layout = [
@@ -43,89 +44,93 @@ while True:
 
 
 
-def iniciar_driver():
-    chrome_options = Options()
-    arguments = ['--lang=pt-BR', '--window-size=1920,1080', '--incognito']
-    for argument in arguments:
-        chrome_options.add_argument(argument)
+    def iniciar_driver():
+        chrome_options = Options()
+        arguments = ['--lang=pt-BR', '--window-size=1920,1080', '--incognito']
+        for argument in arguments:
+            chrome_options.add_argument(argument)
 
-    chrome_options.add_experimental_option('prefs', {
-        'download.prompt_for_download': False,
-        'profile.default_content_setting_values.notifications': 2,
-        'profile.default_content_setting_values.automatic_downloads': 1,
+        chrome_options.add_experimental_option('prefs', {
+            'download.prompt_for_download': False,
+            'profile.default_content_setting_values.notifications': 2,
+            'profile.default_content_setting_values.automatic_downloads': 1,
 
-    })
-    driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install()), options=chrome_options)
+        })
+        driver = webdriver.Chrome(service=ChromeService(
+            ChromeDriverManager().install()), options=chrome_options)
 
-    return driver
+        return driver
 
-
-driver = iniciar_driver()
-# Entrar na página
-driver.get('https://www.olx.com.br/')
-sleep(5)
-
-# Localizar campo de busca
-campo_busca = driver.find_element(By.ID,"searchtext-input")
-# Clicando no campo de busca
-campo_busca.click()
-sleep(2)
-#Digitando no campo de pesquisa
-
-campo_busca.send_keys('Yamaha mt-07')
-sleep(2)
-
-# Achar campo busca em buscar
-botao_busca = driver.find_element(By.CLASS_NAME, 'search-button-submit')
-sleep(2)
-# Clicando no botão de busca
-botao_busca.click()
-sleep(8)
-
-# Xpath dos EStados:
-
-al = driver.find_element(By.XPATH, "//a[text()='Alagoas']") 
-al.click()
-sleep(5)
-
-while True:
+    def digitar_naturalmente(texto, elemento):
+        for letra in texto:
+            elemento.send_keys(pesquisa)
+            sleep(random.randint(1, 5)/30)
 
 
+    driver = iniciar_driver()
+    # Entrar na página
+    driver.get('https://www.olx.com.br/')
+    sleep(5)
 
-    # Não esquecer de mover até o final da pagina.
-    driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+    # Localizar campo de busca
+    campo_busca = driver.find_element(By.ID,"searchtext-input")
+    # Clicando no campo de busca
+    campo_busca.click()
+    sleep(2)
+    
+    #Digitando no campo de pesquisa
+    campo_busca.send_keys(pesquisa)
     sleep(2)
 
+    # Achar campo busca em buscar
+    botao_busca = driver.find_element(By.CLASS_NAME, 'search-button-submit')
+    sleep(2)
+    # Clicando no botão de busca
+    botao_busca.click()
+    sleep(8)
 
-    # Class anuncio normal: kgl1mq-0 eFXRHn sc-ifAKCX iUMNkO
-    # Class anuncio destaque: kgl1mq-0 eFXRHn sc-ifAKCX ghBVzA   
-    # São classes diferentes, preciso achar algo incomum
-    # Achei esta: //div[@class='sc-12rk7z2-7 kDVQFY'] 
-    # Como queremos os titulos pra jogar na tabela, é só ir na tag mãe h2 desta forma; //div[@class='sc-12rk7z2-7 kDVQFY']//h2
+    # Xpath dos EStados:
 
-    titulos = driver.find_elements(By.XPATH,"//div[@class='sc-12rk7z2-7 kDVQFY']//h2")
+    br = driver.find_element(By.XPATH, "//a[text()='Brasil']") 
+    br.click()
+    sleep(5)
+    
+    
 
-    # Agora devemos pesquisar os preços:
-
-    precos = driver.find_elements(By.XPATH, "//div[@class='sc-1kn4z61-1 dGMPPn']")
-
-    links = driver.find_elements(By.XPATH, "//a[@data-lurker-detail='list_id']")
-
-    # estados_todos = driver.find_elements(By.XPATH, '//a[@class="sc-1l6qrj6-0 hSmLZl sc-gzVnrw kGFTcZ"]') 
+    while True:
+        # Não esquecer de mover até o final da pagina.
+        driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+        sleep(2)
 
 
-    # Guardar esses dados em arquivos CSV
-    for titulo, preco, link in zip(titulos, precos, links):
-        with open('precos1.csv', 'a', encoding='utf-8', newline='') as arquivo:
-            link_processado = link.get_attribute('href')   # href é o atributo onde se encontra de fato o link
-            arquivo.write(f'{titulo.text};{preco.text};{link_processado}{os.linesep}')  #os.linesep é uma quebra de linha para organizar as linhas
-    try:        
-        proxima_pagina = driver.find_element(By.XPATH, "//span[text()='Próxima pagina']")     
-        proxima_pagina.click()
-    except:
-        print('Fim da extração')
-        break      
+        # Class anuncio normal: kgl1mq-0 eFXRHn sc-ifAKCX iUMNkO
+        # Class anuncio destaque: kgl1mq-0 eFXRHn sc-ifAKCX ghBVzA   
+        # São classes diferentes, preciso achar algo incomum
+        # Achei esta: //div[@class='sc-12rk7z2-7 kDVQFY'] 
+        # Como queremos os titulos pra jogar na tabela, é só ir na tag mãe h2 desta forma; //div[@class='sc-12rk7z2-7 kDVQFY']//h2
 
-input('')
-driver.close()
+        titulos = driver.find_elements(By.XPATH,"//div[@class='sc-12rk7z2-7 kDVQFY']//h2")
+
+        # Agora devemos pesquisar os preços:
+
+        precos = driver.find_elements(By.XPATH, "//div[@class='sc-1kn4z61-1 dGMPPn']")
+
+        links = driver.find_elements(By.XPATH, "//a[@data-lurker-detail='list_id']")
+
+        # estados_todos = driver.find_elements(By.XPATH, '//a[@class="sc-1l6qrj6-0 hSmLZl sc-gzVnrw kGFTcZ"]') 
+
+
+        # Guardar esses dados em arquivos CSV
+        for titulo, preco, link in zip(titulos, precos, links):
+            with open('precos1.csv', 'a', encoding='utf-8', newline='') as arquivo:
+                link_processado = link.get_attribute('href')   # href é o atributo onde se encontra de fato o link
+                arquivo.write(f'{titulo.text};{preco.text};{link_processado}{os.linesep}')  #os.linesep é uma quebra de linha para organizar as linhas
+        try:        
+            proxima_pagina = driver.find_element(By.XPATH, "//span[text()='Próxima pagina']")     
+            proxima_pagina.click()
+        except:
+            print('Fim da extração')
+            break      
+
+    input('')
+    driver.close()
